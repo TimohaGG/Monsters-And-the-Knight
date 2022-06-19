28cd495379e0b24c20f6c58d5402274787b2427e
Tim <timoha22.a11@gmail.com>
2022-06-19 18:39:12 +0200

    Adding Entity Code

---
+++ Core/Entities/Entity/Entity.cpp
@@ -0,0 +1,47 @@
+#include "Entity.h"
+
+
+Entity::Entity()
+{
+	name = "unknown";
+	hp = 0;
+	damage = 0;
+	gold = 0;
+	skin = '0';
+	isKilled = false;
+}
+
+bool Entity::IsKilled()
+{
+	return isKilled;
+}
+
+int Entity::DecreaseHP(int damage)
+{
+	return hp -= damage;
+}
+
+int Entity::AddGold(int enemyGold)
+{
+	return gold += enemyGold;
+}
+
+void Entity::PrintInfo()
+{
+	cout << "Существо: \t" << name << "\n";
+	cout << "Hp: \t\t" << hp << "\n";
+	cout << "Damage: \t" << damage << "\n";
+	cout << "Gold: \t\t" << gold << "\n";
+	cout << "Skin: \t\t" << skin << "\n";
+	cout << "X: " << x << "\tY: " << y << "\n\n";
+}
+
+
+
+char Entity::ChangeSkin()
+{
+	cout << "Введите новый символ для существа " << name << "\n";
+	char newSkin;
+	cin >> skin;
+	return skin;
+}
---
+++ Core/Entities/Entity/Entity.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "../../Lib/Lib.h"
+
+class Entity {
+public:
+	//----------constructor----------
+	Entity(string name, int hp, int damage, int gold, char skin)
+		:name(name), hp(hp), damage(damage), gold(gold), skin(skin), isKilled(false) {}
+	Entity();
+
+	//----------getters----------
+	int GetHp() { return hp; }
+	int GetDamage() { return damage; }
+	int GetGold() { return gold; }
+	char GetSkin() { return skin; }
+	string GetName() { return name; }
+
+
+
+	//----------methods----------
+	bool IsKilled();
+	char ChangeSkin();
+	int DecreaseHP(int damage);
+	int AddGold(int enemyGold);
+	void PrintInfo();
+
+	int x;
+	int y;
+protected:
+	string name;
+	int hp;
+	int damage;
+	int gold;
+	char skin;
+	bool isKilled;
+
+};
